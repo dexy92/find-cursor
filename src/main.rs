07026245741402cbe449061ac880083079474de9
neut92 <101744964+neuto92@users.noreply.github.com>
2022-06-05 14:52:15 +0200

    Show cursor position for active screen

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,71 @@
 use winit::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
-    window::{WindowBuilder, Fullscreen}, 
-    dpi::PhysicalPosition
+    window::{WindowBuilder, Fullscreen, Window}, 
+    dpi::{PhysicalPosition, PhysicalSize}
 };
+use pixels::{Pixels, SurfaceTexture, wgpu::Color};
 
 
-fn run_loop(event_loop: EventLoop<()>) {
+fn draw(frame: &mut [u8], cursor_position: PhysicalPosition<f64>, window_size: PhysicalSize<u32>){
+    let cursor_x = cursor_position.x as i16;
+    let cursor_y = cursor_position.y as i16;
+    
+    for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
+        let x = (i % window_size.width as usize) as i16;
+        let y = (i / window_size.width as usize) as i16;
+
+        let rgba = if x == cursor_x || y == cursor_y {
+            [0xff, 0x00, 0x00, 0xff]
+            
+        } else{
+            [0x00, 0x00, 0x00, 0x00]
+        };
+
+        pixel.copy_from_slice(&rgba);
+    }
+}
+
+
+fn run_loop(event_loop: EventLoop<()>, window: Window) {
+    let window_size = window.inner_size();
+    let mut pixels = {
+        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+        Pixels::new(window_size.width , window_size.height, surface_texture)
+    }.unwrap();
+    pixels.set_clear_color(Color::TRANSPARENT);
+
     let mut cursor_position = PhysicalPosition::new(0.0, 0.0);
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
 
-        println!("{:?}", cursor_position);
-        
         match event {
             Event::WindowEvent {event, ..} => match event{
                 WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-                WindowEvent::CursorMoved { position, .. } => cursor_position = position,
+                WindowEvent::CursorMoved { position, .. } => {
+                    cursor_position = position; 
+                    window.request_redraw();
+                },
                 _ => ()
             }
+            Event::RedrawRequested(_) => {
+                draw(pixels.get_frame(), cursor_position, window_size); 
+                pixels.render().expect("Render failed");
+            }
             _ => ()
-        }
-        
+        }        
     });
 }
 
 
 fn main() {
     let event_loop = EventLoop::new();
-    let _window = WindowBuilder::new().with_fullscreen(Some(Fullscreen::Borderless(None))).build(&event_loop).unwrap();
+    let window = WindowBuilder::new()
+        .with_transparent(true)
+        .with_decorations(false)
+        .with_fullscreen(Some(Fullscreen::Borderless(None)))
+        .build(&event_loop).unwrap();
 
-    run_loop(event_loop);
+    run_loop(event_loop, window);
 }
