52a83d3036a2162b09369f18cf2cf98596dd1e4b
neut92 <101744964+neuto92@users.noreply.github.com>
2022-06-05 13:48:26 +0200

    Print cursor position

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,35 @@
 use winit::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
-    window::{WindowBuilder, Fullscreen}
+    window::{WindowBuilder, Fullscreen}, 
+    dpi::PhysicalPosition
 };
 
-fn main() {
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().with_fullscreen(Some(Fullscreen::Borderless(None))).build(&event_loop).unwrap();
+
+fn run_loop(event_loop: EventLoop<()>) {
+    let mut cursor_position = PhysicalPosition::new(0.0, 0.0);
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
 
+        println!("{:?}", cursor_position);
+        
         match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                window_id,
-            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
-            _ => (),
+            Event::WindowEvent {event, ..} => match event{
+                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                WindowEvent::CursorMoved { position, .. } => cursor_position = position,
+                _ => ()
+            }
+            _ => ()
         }
+        
     });
+}
+
+
+fn main() {
+    let event_loop = EventLoop::new();
+    let _window = WindowBuilder::new().with_fullscreen(Some(Fullscreen::Borderless(None))).build(&event_loop).unwrap();
+
+    run_loop(event_loop);
 }
